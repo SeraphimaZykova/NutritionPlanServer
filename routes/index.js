16c79b6be439997981cf20ccef58db7eeedbfbca
Serafima Zykova <zykovas91@gmail.com>
2018-01-02 01:29:57 +0300

    mongoose connection

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const
     express = require('express')
+  , mongoose = require('mongoose') 
   , router = express.Router()
   ;
+
+const dbURI = 'mongodb://nutritionReader:nutritionreader@localhost:27017/NutritionPlan?authSource=admin'
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ' + dbURI);
+}); 
+
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function() {  
+  mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}); 
 
 const GAG = [
   {
