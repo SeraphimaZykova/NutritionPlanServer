60562321109079bc8c99d5a594c07407adcf24f1
Serafima Zykova <zykovas91@gmail.com>
2018-02-03 16:00:00 +0300

    handling client requests

--- routes/index.js
+++ routes/index.js
@@ -2,55 +2,87 @@ const
     express = require('express')
   , mongo = require('./../database/mongoManager')
   , router = express.Router()
+  , HARDCODED_USER_ID = '5a4aafeae02a03d8ebf35361'
   ;
 
-async function makeDbRequest(response) {
-  // let query = UserData.findById(HARDCODED_USER_ID);
-
-  // query.then((res) => {
-  //   let fullUserfoodArray = []
-  //     ;
 let handleError = (routerRes, code, info) => {
   console.error('Error: ' + code + ' -> ' + info);
   routerRes.send(code);
 }
 
-  //   Promise.all(res.pantry.map((fObj) => {
-  //     return Food.findById(fObj.foodId).then(
-  //       rslv => {
-  //         return new Promise((resolve, reject) => {
-  //           let nFdObj = {}
-  //             ;
-  //           nFdObj.mInfo = rslv;
-  //           nFdObj.uInfo = fObj;
+let createClientPatryData = (foodObj, pantryObj, reqUserId) => {
+  return new Promise((resolve, reject) => {
+    if (foodObj) {
+      let foodstuff = {};
+      foodstuff.foodInfo = foodObj;
+      foodstuff.pantryInfo = pantryObj;
+      foodstuff.userInfo = {
+        userId: reqUserId
+      }
 
-  //           resolve(nFdObj);
-  //         });
-  //       }
-  //     );
-  //   })).then(
-  //     rslv => {
-  //       response.send(rslv);
-  //     }
-  //   )
-  // });
+      resolve(foodstuff);
+    }
+    else {
+      reject(`Unknown food id`);
+    }
+  });
 }
 
-//dbInit();
-
-let handleError = (routerRes, code, info) => {
-  console.error('Error: ' + code + ' -> ' + info);
-  routerRes.send(code);
+let modifyPantryObj = (pantryObj, userId) => {
+  return mongo.getFood(pantryObj.foodId)
+  .then(food => {
+    return createClientPatryData(food, pantryObj, userId);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 }
 
-router.get('/test', (req, res, next) => {
-  makeDbRequest(res);
-})
+let removeUndef = (array) => {
+  let undefIndex = array.indexOf(undefined);
+  while (undefIndex != -1) {
+    array.splice(undefIndex, 1);
+    undefIndex = array.indexOf(undefined);
+  }
+  return array;
+}
 
+let test1 = () => {
+  console.log('test1');
+  
+}
+
+setTimeout(test1, 1000);
+
+async function requestPantry(response) {
+  mongo.getPantry(HARDCODED_USER_ID)
+  .then(pantry => {
+    Promise.all(pantry.map((fObj) => {
+      return modifyPantryObj(fObj, HARDCODED_USER_ID);
+    })).then(rslv => {
+      rslv = removeUndef(rslv);
+      response.send(rslv);
+    });
+  }) 
+  .catch(err => {
+    console.log(`error: ${err}`);
+    handleError(responce, 200, err);
+  });
+}
+
+async function updatePantry(responce, userId, pantryObj) {
+  mongo.updatePantry(userId, pantryObj)
+  .then(result => {
+    responce.send(result);
+  })
+  .catch(err => {
+    console.log(err);
+    handleError(responce, 200, err);
+  });
+}
 
 router.get('/foods', function(req, res, next) {
-  makeDbRequest(res);
-  //res.send(getFoodsList());
+  requestPantry(res);
 });
 
 let test = () => {
@@ -61,7 +93,7 @@ let test = () => {
     'available': 0,
     'daily': {
       'min': 0,
-      'max': 100
+      'max': 300
     },
     'nutrition': {
       'calories': 65,
@@ -134,38 +166,8 @@ router.post('/newFood', (req, res) => {
 });
 
 router.post('/updateUserInfo', (req, res) => {
-  let REC_DATA = req.body
-    ;
-
-  // UserData.findById(HARDCODED_USER_ID, function(err, doc) {
-  //   if (err) {
-  //     handleError(res, 200, {});
-  //     return;
-  //   }
-
-  //   doc.pantry.some(element => {
-  //     if (element.foodId == REC_DATA.uInfo.foodId) {
-  //       element.set({
-  //         delta: REC_DATA.uInfo.delta,
-  //         available: REC_DATA.uInfo.available,
-  //         daily: REC_DATA.uInfo.daily
-  //       });
-
-  //       doc.save(function(err, updatedRes) {
-  //         if (err) {
-  //           handleError(res, 200, {});
-  //           return;
-  //         }
-
-  //         res.sendStatus(400);
-  //       });
-
-  //       return true;
-  //     }
-
-  //     return false;
-  //   });
-  // });
+  let REC_DATA = req.body;
+  updatePantry(res, REC_DATA.userInfo.userId, REC_DATA.pantryInfo);
 });
 
 module.exports = router;
