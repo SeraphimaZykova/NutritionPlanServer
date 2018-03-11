1cfa5cbea4fcef26f8a7c5027381f778e63f0766
Serafima Zykova <zykovas91@gmail.com>
2018-03-11 11:20:59 +0300

    added ration creation

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const
   , router = express.Router()
   , HARDCODED_USER_ID = '5a4aafeae02a03d8ebf35361'
   , nutritionix = require('../api/nutritionix')
+  , ration = require('ration_calculator');
   ;
 
 let handleError = (routerRes, code, info) => {
@@ -61,7 +62,7 @@ let modifyPantryObj = (pantryObj, userId) => {
 }
 
 let modifyPantryForRation = (pantryObj) => {
-  return mongo.getFood(pantryObj.foodId, { 'nutrition': 1, 'glycemicIndex': 1 })
+  return mongo.getRationFood(pantryObj.foodId)
   .then(food => {
     return createRationPantryData(food, pantryObj);
   })
@@ -102,34 +103,46 @@ function requestPantry(response) {
 
 function requestRation(response) {
   mongo.getIdealNutrition(HARDCODED_USER_ID)
-  .then(nutrition => {
-    nutrition.proteins = nutrition.calories * nutrition.proteins;
-    nutrition.fats = nutrition.calories * nutrition.fats;
-    nutrition.carbs = nutrition.calories * nutrition.carbs;
+  .then(idealNutrition => {
+    idealNutrition.proteins = idealNutrition.calories * idealNutrition.proteins;
+    idealNutrition.fats = idealNutrition.calories * idealNutrition.fats;
+    idealNutrition.carbs = idealNutrition.calories * idealNutrition.carbs;
 
     mongo.getPantry(HARDCODED_USER_ID)
     .then(pantry => {
       Promise.all(pantry.map((fObj) => {
         return modifyPantryForRation(fObj, HARDCODED_USER_ID);
-      })).then(rslv => {
-        rslv = removeUndef(rslv);
+      })).then(rationFoods => {
+        rationFoods = removeUndef(rationFoods);
+        
+        console.dir(rationFoods);
 
-        console.dir(rslv);
-        response.send(rslv);
+        ration.calculateRation(idealNutrition, rationFoods)
+        .then(res => {
+          console.log(`result: ${res}`);
+          response.send(res);
+        })
+        .catch(err => {
+          console.log(`calculate ration error: ${err}`);
+          handleError(response, 200, err);
+        });
       })
       .catch(err => {
-        console.log(`error: ${err}`);
+        console.log(`pantry array ration modification error: ${err}`);
         handleError(response, 200, err);
+        return;
       });
     }) 
     .catch(err => {
-      console.log(`error: ${err}`);
+      console.log(`get pantry error: ${err}`);
       handleError(response, 200, err);
+      return;
     });
-  }) 
+  })
   .catch(err => {
-    console.log(`error: ${err}`);
+    console.error(`get ideal nutrition error: ${err}`);
     handleError(response, 200, err);
+    return;
   });
 }
 
