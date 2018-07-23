dd9d97a4e760b91dc8dfde383337ba458b86f185
Serafima Zykova <zykovas91@gmail.com>
2018-07-23 17:57:48 +0300

    removed test test

--- test/serverTest.js
+++ test/serverTest.js
@@ -17,29 +17,6 @@ describe('server', function () {
     www.stop();
   });
 
-  describe('/test', function () {
-    it('should return 200', function (done) {
-      http.get(httpRequest + '/test', function (res) {
-        assert.equal(200, res.statusCode);
-        done();
-      });
-    });
-  
-    it('should say "Hello, world!"', function (done) {
-      http.get(httpRequest + '/test', function (res) {
-        var data = '';
-  
-        res.on('data', function (chunk) {
-          data += chunk;
-        });
-  
-        res.on('end', function () {
-          assert.equal('Hello, world!\n', data);
-          done();
-        });
-      });
-    });
-  });
 
   describe('/search', function() {
     it('invalid request, return error code 400', function(done) {
